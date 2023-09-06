03430e7e76fee6f9b58e25e2883a5329defad9fb
Paul Hoguth <paul.hoguth@firstwaters.de>
2023-09-06 13:34:39 +0200

    Added Median, Percentile and BoxPlot feature

--- src/main/java/de/teatime08/BanquetteAspect.java
+++ src/main/java/de/teatime08/BanquetteAspect.java
@@ -6,9 +6,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Aspect
 @Component
@@ -73,5 +72,105 @@ public class BanquetteAspect {
             y.forEach(z -> sb.append(String.format("%-40s", x) + z.toString() + "\n"));
         });
         return sb.toString();
+    }
+
+    /**
+     * Calculates the median value for a given key
+     * @param key the key to search for in the measurements
+     * @return the median value of bench times
+     */
+    public double median(String key) {
+        return calculateMedian(banqMeasures.get(key).stream().map(x -> x.benchNanoTime).collect(Collectors.toList()));
+    }
+
+    public static double calculateMedian(List<Long> measurements) {
+        if (measurements.isEmpty()) {
+            return 0.0; // Handle the case of an empty list as appropriate for your use case.
+        }
+
+        Collections.sort(measurements);
+
+        int middle = measurements.size() / 2;
+        if (measurements.size() % 2 == 0) {
+            // If even number of elements, take the average of the two middle values
+            double middle1 = measurements.get(middle - 1);
+            double middle2 = measurements.get(middle);
+            return (middle1 + middle2) / 2.0;
+        } else {
+            // If odd number of elements, return the middle value
+            return measurements.get(middle);
+        }
+    }
+
+    /**
+     * Represents a boxplot.
+     * @param min total minimum value of the boxplot
+     * @param max total maximum value of the boxplot
+     * @param median the median of all values
+     * @param qLow the 25 percentile
+     * @param qHigh the 75 percentile
+     */
+    public record BoxPlot(double min, double max, double median, double qLow, double qHigh){
+        @Override
+        public String toString() {
+            return "BoxPlot{" + "min=" + min + ", max=" + max + ", median=" + median +
+                ", qLow=" + qLow + ", qHigh=" + qHigh + '}';
+        }
+    }
+
+    /**
+     * Creates a boxplot from the measurement values.
+     * @param key the key to search for in the measurements
+     * @return a boxplot
+     */
+    public BoxPlot boxPlot(String key) {
+        return createBoxPlot(banqMeasures.get(key).stream().map(x -> x.benchNanoTime).collect(Collectors.toList()));
+    }
+
+    public static BoxPlot createBoxPlot(List<Long> measurements) {
+        Collections.sort(measurements);
+
+        double min = measurements.get(0);
+        double max = measurements.get(measurements.size() - 1);
+        double median = calculateMedian(measurements);
+        double q1 = calculatePercentile(measurements, 25);
+        double q3 = calculatePercentile(measurements, 75);
+
+        return new BoxPlot(min, max, median, q1, q3);
+    }
+
+    /**
+     * Calculates the value for percentile of the measurements
+     * @param key the key to search for in the measurements
+     * @param percentile the percentile to be measured at
+     * @return percentile
+     */
+    public double percentile(String key, int percentile) {
+        return calculatePercentile(
+            banqMeasures.get(key).stream().map(x -> x.benchNanoTime).collect(Collectors.toList()),
+            percentile
+        );
+    }
+
+    public static double calculatePercentile(List<Long> measurements, int percentile) {
+        if (measurements.isEmpty()) {
+            return 0.0; // Handle the case of an empty list as appropriate for your use case.
+        }
+
+        Collections.sort(measurements);
+
+        double position = (percentile / 100.0) * (measurements.size() + 1);
+        int index = (int) position;
+        double fraction = position - index;
+
+        if (index == 0) {
+            return measurements.get(0);
+        } else if (index >= measurements.size()) {
+            return measurements.get(measurements.size() - 1);
+        } else {
+            double lowerValue = measurements.get(index - 1);
+            double upperValue = measurements.get(index);
+            return lowerValue + fraction * (upperValue - lowerValue);
+        }
     }
 }
